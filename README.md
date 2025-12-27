5d955b9630c5d90eeff961b2df57b937af0c4a79
maeve <myosumonaung91@gmail.com>
2025-12-27 23:40:35 +0900

    online shop

---
+++ README.md
@@ -0,0 +1,1 @@
+A Bootstrap Online Shop Project
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+    session_start(); // use for session
+    ob_start(); // use for page navigation
+
+    define('SITEURL', 'http://localhost/OS/');
+    $sever_name = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "os_db";
+
+    // Create connection in mysqli
+    $conn = mysqli_connect($sever_name, $username, $password, $dbname);
+
+    if ($conn) {
+        // echo "connected";
+    }else{
+        die("Error on the connection");
+    }
+?>
